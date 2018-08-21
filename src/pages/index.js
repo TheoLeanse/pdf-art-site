7e4ce59c54da81d07df17b4512cfa8997fdc651d
theoleanse <theoleanse@gmail.com>
2018-08-21 19:03:03 +0100

    remove graphql query from empty index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,5 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-const IndexPage = ({ data }) => <div />
+const IndexPage = () => <div />
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query HomePageQuery {
-    markdownRemark {
-      frontmatter {
-      }
-    }
-  }
-`
